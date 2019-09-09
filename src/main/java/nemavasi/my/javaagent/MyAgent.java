02cb0e535c1d5cedae88f61d92aa2fc14fb681be
dvshalygin <thatbook2@gmail.com>
2019-09-09 15:57:24 +0300

    init

---
+++ src/main/java/nemavasi/my/javaagent/MyAgent.java
@@ -0,0 +1,8 @@
+package nemavasi.my.javaagent;
+
+public class MyAgent {
+    public static void premain(String arg) {
+        System.out.println("Agent is starting...");
+
+    }
+}
---
+++ src/test/java/nemavasi/my/javaagent/MyAgentTest.java
@@ -0,0 +1,9 @@
+package nemavasi.my.javaagent;
+
+import java.lang.instrument.Instrumentation;
+
+public class MyAgentTest {
+    public static void main(String[] args) {
+        System.out.println("App is starting...");
+    }
+}
