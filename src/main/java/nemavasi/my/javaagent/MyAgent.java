e63b8f2e91d9ef2135c5df407a0c13e786126cd1
dvshalygin <thatbook2@gmail.com>
2019-09-09 16:31:53 +0300

    loaded classes count

--- src/main/java/nemavasi/my/javaagent/MyAgent.java
+++ src/main/java/nemavasi/my/javaagent/MyAgent.java
@@ -3,9 +3,21 @@ package nemavasi.my.javaagent;
 import java.lang.instrument.Instrumentation;
 
 public class MyAgent {
+
+    private static Instrumentation instrumentation;
+
     public static void premain(String arg, Instrumentation instrumentation) {
+        MyAgent.instrumentation = instrumentation;
+
         System.out.println("Agent is starting...");
         instrumentation.addTransformer(new ClassTransformer());
+    }
 
+    public static long objSize(Object object){
+        if (instrumentation == null) {
+            throw new RuntimeException("Агент не инициилизирован ");
+        }
+
+        return instrumentation.getObjectSize(object);
     }
 }
--- src/test/java/nemavasi/my/javaagent/MyAgentTest.java
+++ src/test/java/nemavasi/my/javaagent/MyAgentTest.java
@@ -1,7 +1,25 @@
 package nemavasi.my.javaagent;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Locale;
+
 public class MyAgentTest {
     public static void main(String[] args) {
         System.out.println("App is starting...");
+        printObjectSize(new Object());
+        printObjectSize(new Locale("ru"));
+        printObjectSize(1);
+        printObjectSize("string");
+        printObjectSize(Calendar.getInstance());
+        printObjectSize(new BigDecimal("999999999999999.999"));
+        printObjectSize(new ArrayList<String>());
+        printObjectSize(new Integer[100]);
+
+    }
+
+    public static void printObjectSize(Object obj) {
+        System.out.println(String.format("%s, size=%s", obj.getClass().getSimpleName(), MyAgent.objSize(obj)));
     }
 }
